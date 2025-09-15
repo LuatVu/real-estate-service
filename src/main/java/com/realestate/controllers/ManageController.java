68819763d706fae22a5682eceef02dd772a2c92f
Luatvc <luat.vc.tb@gmail.com>
2025-09-15 23:17:33 +0700

    update Manage Post api

--- src/main/java/com/realestate/controllers/ManageController.java
+++ src/main/java/com/realestate/controllers/ManageController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +24,7 @@ public class ManageController {
     @Autowired
     private PostService postService;
 
-    @GetMapping("/get-posts")
+    @PostMapping("/get-posts")
     public ResponseEntity<ApiResponseDto<?>> getPosts(@RequestBody PostRequest postRequest) {
         try {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
